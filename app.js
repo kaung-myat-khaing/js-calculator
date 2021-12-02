324a2ec724ce4956d1e2e7124bc8369fc631a498
kaung-myat-khaing <kaung8701@gmail.com>
2021-12-03 01:37:01 +0630

    fix minor bugs

--- app.js
+++ app.js
@@ -1,26 +1,47 @@
 let keypad = document.querySelector(".keypad");
 let displayBox = document.querySelector(".display-box");
+let entities, operators;
 //displaying numbers in the display as they clicked
 keypad.addEventListener("click", e => {
-    let entities = ["+", "−", "×", "÷"];
-    let operators = ["+", "-", "*", "/"];
+    entities = ["+", "−", "×", "÷", "%"];
+    operators = ["+", "-", "*", "/", "%"];
     if (e.target.innerText === "AC") {
         displayBox.innerText = "";
     }
     else if (e.target.innerText === "C") {
-        displayBox.innerText = displayBox.innerText.slice(0,displayBox.innerText.length-1);
+        clearLast();
     }
     // Performing calculation as the "=" key is pressed and displaying the result
     else if (e.target.innerText === "=") {
         for (let char of displayBox.innerText){
-            for(let i=0; i<entities.length; i++){
-            if(isNaN(char) && char == entities[i])displayBox.innerText=displayBox.innerText.replace(char, operators[i]);   
-            }  
+            entities.forEach((element, index) => {if (char == element) 
+                displayBox.innerText = displayBox.innerText.replace(char, operators[index])
+            });
+        }
+        if (!operators.includes(displayBox.innerText[displayBox.innerText.length - 1])) {
+            displayBox.innerText = eval(displayBox.innerText)
         }
         
-        displayBox.innerText = eval(displayBox.innerText).toFixed(5);
-    } 
+    }
     else display(e.target.innerText);
+    if (displayBox.innerText.length < 5) displayBox.style.fontSize = 90 + "px";
+    else displayBox.style.fontSize = 75 + "px";
 });
+//function for "C" key press
+let clearLast = _ => displayBox.innerText = displayBox.innerText.slice(0, displayBox.innerText.length - 1);
+
+
+let filter = x => {
+    let current = displayBox.innerText;
+    let lastChar = current[current.length - 1];
+
+    if (current.length >= 9) return false;
+
+    if (current == "0" && x != ".") clearLast();
+    if (operators.includes(x) && operators.includes(lastChar)) return false;
+    return true;
+}
 //function for displaying keypresses
-let display = (buttonText) => displayBox.innerText += buttonText;
+let display = (buttonText) => {
+    if (filter(buttonText)) displayBox.innerText += buttonText;
+}
